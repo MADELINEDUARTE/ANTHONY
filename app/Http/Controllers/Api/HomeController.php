9034243d5e6a4acac6082dacd299699c59d842cc
Juan Guerra <juancarlosguerrawar@gmail.com>
2022-02-01 16:59:58 -0300

    Inicio Api

---
+++ app/Http/Controllers/Api/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Program;
+
+class HomeController extends Controller
+{
+    public function index(Request $request){
+        //dd($request->all());
+        $programs = Program::paginate(1);
+        
+        return response()->json(['status'=> true, 'programs'=> $programs]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::middleware('token_validate')->group(function(){
+    Route::get('home',[HomeController::class,'index']);
+});
+
