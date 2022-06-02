24ed53618d3b8c7c765d523749e56970a6f9d7b0
Juan Guerra <juancarlosguerrawar@gmail.com>
2022-06-02 20:56:13 -0300

    Ajuste Ruta Web

--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('auth.login');
 });
+*/
+
+Route::get('/', \Filament\Http\Livewire\Auth\Login::class);
 
 /*
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
