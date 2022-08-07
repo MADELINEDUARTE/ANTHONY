e6ceea7cd5ceaf6d45967cc8facd053e50c5d0f4
yosecc <yosec.cervino@gmail.com>
2022-08-06 23:36:36 -0300

    pusher

--- app/Listeners/StripeEventListener.php
+++ app/Listeners/StripeEventListener.php
@@ -8,6 +8,7 @@ use Laravel\Cashier\Events\WebhookReceived;
 use App\Models\Subscription;
 use App\Models\User;
 use App\Models\Package;
+use App\Events\SendPusher;
 
 
 class StripeEventListener
@@ -23,18 +24,32 @@ class StripeEventListener
         if ($event->payload['type'] === 'invoice.payment_succeeded') {
             $user = User::where('stripe_id',$event->payload['data']['object']['customer'])->first();
             if($user){
-$package = Package::where('stripe_id',$event->payload['data']['object']['lines']['data'][0]['price']['product'])->first();
+                $package = Package::where('stripe_id',$event->payload['data']['object']['lines']['data'][0]['price']['product'])->first();
                 $subscription = Subscription::where('user_id',$user->id)->latest()->first();
                 $subscription->package_id = $package->id;
-                
                 $subscription->save();
+
+                $data = [
+                  'error'   => true,
+                  'message' => 'Subscription processed. Press OK to continue',
+                  'user_id' => $user->id
+                ];
+                event(new SendPusher($data));
                
             }else{
-                 \Log::info('============ customer.subscription.created');
-                 \Log::info('Nsot user');
-                 \Log::info('==========================================');
-
+              \Log::info('============ customer.subscription.created');
+              \Log::info('Nsot user');
+              \Log::info('==========================================');
             }
         }
+        // if ($event->payload['type'] === 'invoice.payment_failed') {
+        //   $user = User::where('stripe_id',$event->payload['data']['object']['customer'])->first();
+        //   $data = [
+        //     'error'   => false,
+        //     'message' => 'Error',
+        //     'user_id' => $user->id
+        //   ];
+        //   event(new SendPushNotification($data));
+        // }
     }
 }
