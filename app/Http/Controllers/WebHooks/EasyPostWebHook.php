18dd345955a82baf6028d023a4c23816c9570397
yosecc <yosec.cervino@gmail.com>
2022-09-17 14:39:42 -0300

    Probando array

--- app/Http/Controllers/WebHooks/EasyPostWebHook.php
+++ app/Http/Controllers/WebHooks/EasyPostWebHook.php
@@ -9,14 +9,14 @@ use Illuminate\Http\Request;
 class EasyPostWebHook extends Controller
 {
     public function index(Request $request){
-        \Log::info($request);
-        if($request->description == 'tracker.updated'){
-            
-            Shipment::where('tracker_id',$request->result['shipment_id'])->update([
-              'tracker_status'        => $request->result['status'],
-              'tracker_signed_by'     => $request->result['signed_by'],
-              'tracker_carrier'       => $request->result['carrier'],
-              'tracker_public_url'    => $request->result['public_url'],
+        
+        if($request['description'] == 'tracker.updated'){
+            \Log::info('Llego hasta aqui');
+            Shipment::where('tracker_id',$request['result']['shipment_id'])->update([
+              'tracker_status'        => $request['result']['status'],
+              'tracker_signed_by'     => $request['result']['signed_by'],
+              'tracker_carrier'       => $request['result']['carrier'],
+              'tracker_public_url'    => $request['result']['public_url'],
             ]);
 
         }
