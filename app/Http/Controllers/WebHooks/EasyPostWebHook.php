2d4e02eb60b672e5231cedcf88c1d3cf248fe5a9
yosecc <yosec.cervino@gmail.com>
2022-09-17 14:41:05 -0300

    returnbody

--- app/Http/Controllers/WebHooks/EasyPostWebHook.php
+++ app/Http/Controllers/WebHooks/EasyPostWebHook.php
@@ -12,14 +12,14 @@ class EasyPostWebHook extends Controller
         
         if($request['description'] == 'tracker.updated'){
             \Log::info('Llego hasta aqui');
-            Shipment::where('tracker_id',$request['result']['shipment_id'])->update([
+            $shipment = Shipment::where('tracker_id',$request['result']['shipment_id'])->update([
               'tracker_status'        => $request['result']['status'],
               'tracker_signed_by'     => $request['result']['signed_by'],
               'tracker_carrier'       => $request['result']['carrier'],
               'tracker_public_url'    => $request['result']['public_url'],
             ]);
-
+          return response()->json(['status'=> true, 'shipment_id' => $shipment->id]);
         }
-        return true;
+        reurn;
     }
 }
