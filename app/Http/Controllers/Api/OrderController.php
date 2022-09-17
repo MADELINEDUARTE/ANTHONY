39b35bec2a288580810f178044ff1955c3842929
yosecc <yosec.cervino@gmail.com>
2022-09-17 13:08:26 -0300

    fleca error

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -41,7 +41,7 @@ class OrderController extends Controller
   public function index()
   {
     $user = Auth::user();
-    $orders = Orders::->latest()->get();
+    $orders = Orders::latest()->get();
     
     return response()->json($orders);
   }
