be8bc553db97f2fc27f6d95467b3f31af33d7db0
yosecc <yosec.cervino@gmail.com>
2022-09-18 12:37:37 -0300

    save

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -11,12 +11,17 @@ class Orders extends Model
 
     protected $fillable = ['price','status','direccion_envio','stripe_id','envio_easypost_id','price_rate'];
 
-    protected $hidden = ['updated_at','user_id','stripe_id'];
+    protected $hidden = ['updated_at','user_id','stripe_id','envio_easypost_id'];
 
-    protected $with = ['ordersProducts'];
+    protected $with = ['ordersProducts','shipment'];
 
     public function ordersProducts()
     {
         return $this->hasMany(OrdersProducts::class,'order_id');
+    }
+
+    public function shipment()
+    {
+        return $this->hasOne(Shipment::class, 'order_id');
     }
 }
